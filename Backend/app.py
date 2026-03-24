2ab6bc1cd613d2a6c92f543bcd3960ee2ec9de67
srivallinayagam <srivallinayagamvs@gmail.com>
2026-03-24 10:57:51 +0530

    req in backend

--- Backend/app.py
+++ Backend/app.py
@@ -341,6 +341,7 @@ def github_login():
         "profile_pic": user.get("profile_pic", profile_pic)
     }), 200
 
-    
+
 if __name__ == "__main__":
-    app.run(port=5000,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
